f1f839b7668a88f4479c1b525846dbbbd48b206f
Jacek Debski <jacek.debski@orange.pl>
2022-02-05 17:06:22 +0100

    connect new-user-form-component with data.service

--- src/app/new-user-form/new-user-form.component.ts
+++ src/app/new-user-form/new-user-form.component.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
 import {FormControl, Validators} from '@angular/forms';
+import { User } from '../models/user.model';
+import { DataService } from '../services/data.service';
 
 @Component({
     selector: 'app-new-user-form',
@@ -8,4 +10,12 @@ import {FormControl, Validators} from '@angular/forms';
 })
 export class NewUserFormComponent {
   emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  public user: User = new User('','','','','','','');
+  constructor(private dataService: DataService) { }
+
+  validateData(){
+    if(this.user.name != '' && this.user.lastname != '' && this.user.road != '' && this.user.city != "" && this.user.zipcode !=""){
+      this.dataService.user = this.user;
+    }
+  }
 }
